b0880f3f0e73dc526acf3ece4a463d72db5db862
santoshmohan04 <santosh_tagore04@rediffmail.com>
2021-09-15 17:28:09 +0530

    5 files modified

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html'
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  allowNewProducts = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewProducts = true;
+    }, 2000);
+  }
+
+  ngOnInit() {}
+}
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  getserverStatus() {
+    return this.serverStatus;
+  }
+}
