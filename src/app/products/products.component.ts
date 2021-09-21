9d6286367abcb72ca319356471e6abdaf3aa63b6
santoshmohan04 <santosh_tagore04@rediffmail.com>
2021-09-22 00:57:09 +0530

    6 files modified

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  templateUrl: './products.component.html'
+  templateUrl: './products.component.html',
 })
 export class ProductsComponent implements OnInit {
   allowNewProducts = false;
+  usernameCreated = false;
+  username = '';
 
   constructor() {
     setTimeout(() => {
@@ -14,4 +16,16 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
+
+  onCreateUsername() {
+    this.usernameCreated = true;
+  }
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
   serverId = 10;
   serverStatus = 'offline';
+  serverCreationStatus = 'No Server was Created!';
+  serverName = 'Stackblitz';
+  servers = ['TestServer', 'TestServer2']
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getserverStatus() {
     return this.serverStatus;
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was Created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
